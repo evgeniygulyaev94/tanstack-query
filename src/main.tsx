30507304290496580f19efae19fb6a9fb666cb78
evgeniygulyaev94 <evgeniygulyaev94@icloud.com>
2026-09-11 20:13:34 +0300

    styles fixed

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,9 @@ import App from './components/App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { UsersPage } from './components/UsersPage.tsx';
 import { PostsPage } from './components/PostsPage.tsx';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ReactQueryDevtools initialIsOpen={false} />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
